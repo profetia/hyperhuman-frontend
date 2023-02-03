6fddd1ae1c491822bcf860f833be755512a64ad4
Clarivy <zqc.sc.hn@gmail.com>
2023-02-04 00:52:56 +0800

    feat: search button in search bar

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,4 +1,12 @@
-import { Input, Flex, Box, Heading } from '@chakra-ui/react'
+import {
+  Input,
+  Flex,
+  Box,
+  Heading,
+  Button,
+  ButtonGroup,
+  VStack,
+} from '@chakra-ui/react'
 
 const SearchBar = () => {
   return (
@@ -6,18 +14,28 @@ const SearchBar = () => {
       <Flex justify="center">
         <Heading as="h1">Hyperhuman</Heading>
       </Flex>
-      <Flex justify="center" mt={10}>
-        <Box width={600}>
-          <Input
-            placeholder="Search"
-            size="lg"
-            variant="filled"
-            _focus={{
-              borderColor: 'teal.500',
-            }}
-          />
-        </Box>
-      </Flex>
+      <VStack>
+        <Flex justify="center" mt={10}>
+          <Box width={600}>
+            <Input
+              placeholder="Describe the model you want to generate"
+              size="lg"
+              variant="filled"
+              _focus={{
+                borderColor: 'teal.500',
+              }}
+            />
+          </Box>
+        </Flex>
+        <ButtonGroup mx={3}>
+          <Button colorScheme="blue" width={20}>
+            Search
+          </Button>
+          <Button colorScheme="blue" width={20}>
+            Generate
+          </Button>
+        </ButtonGroup>
+      </VStack>
     </Box>
   )
 }
