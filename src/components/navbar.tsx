db53a63f73914d57b5303bbce47113554d9654fc
Clarivy <zqc.sc.hn@gmail.com>
2023-02-05 10:53:18 +0800

    feat: add nav bar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,99 @@
+import { ReactNode } from 'react'
+import {
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+  Image,
+  Heading,
+  HStack,
+  Text,
+} from '@chakra-ui/react'
+import { IoMdMoon as MoonIcon, IoMdSunny as SunIcon } from 'react-icons/io'
+
+const NavLink = ({ children }: { children: ReactNode }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href={'#'}
+  >
+    {children}
+  </Link>
+)
+
+export default function NavBar() {
+  const { colorMode, toggleColorMode } = useColorMode()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Box px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <HStack>
+            <Image src="/deemos.png" alt="Deemos" height="64px" />
+            <Text fontSize="2xl">-</Text>
+            <Heading as="h2" size="lg">
+              HyperHuman Project
+            </Heading>
+          </HStack>
+
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={'full'}
+                  variant={'link'}
+                  cursor={'pointer'}
+                  minW={0}
+                >
+                  <Avatar
+                    size={'sm'}
+                    src={'https://avatars.dicebear.com/api/male/username.svg'}
+                  />
+                </MenuButton>
+                <MenuList alignItems={'center'}>
+                  <br />
+                  <Center>
+                    <Avatar
+                      size={'2xl'}
+                      src={'https://avatars.dicebear.com/api/male/username.svg'}
+                    />
+                  </Center>
+                  <br />
+                  <Center>
+                    <p>Username</p>
+                  </Center>
+                  <br />
+                  <MenuDivider />
+                  <MenuItem>Your Servers</MenuItem>
+                  <MenuItem>Account Settings</MenuItem>
+                  <MenuItem>Logout</MenuItem>
+                </MenuList>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import SearchBar from '@/components/search-bar'
 import CardSession from '@/components/card-session'
+import NavBar from '@/components/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +27,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <NavBar></NavBar>
       <SearchBar></SearchBar>
       <Center mt={10}>
         <Tabs variant="solid-rounded" colorScheme="blue" size="lg">
