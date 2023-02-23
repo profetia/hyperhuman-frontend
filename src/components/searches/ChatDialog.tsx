3cb0bf2b321167f0c58840ab4e5c011213451b83
Clarivy <zqc.sc.hn@gmail.com>
2023-02-23 14:50:28 +0800

    style: apply UI to prompt editable

--- src/components/searches/ChatDialog.tsx
+++ src/components/searches/ChatDialog.tsx
@@ -14,11 +14,14 @@ import {
   HStack,
   Button,
   Code,
+  Text,
   Editable,
   EditableTextarea,
   EditablePreview,
   LightMode,
   Tag,
+  Center,
+  VStack,
 } from '@chakra-ui/react'
 import ChatArea from '@/components/dialogs/ChatArea'
 import ModelView from '@/components/dialogs/ModelView'
@@ -100,7 +103,9 @@ function ChatInputArea(props: Props) {
 }
 
 export default function ChatDialog(props: Props) {
-  const [localPrompt, setLocalPrompt] = useState<string>('')
+  const [localPrompt, setLocalPrompt] = useState<string>(
+    'He has a great smile He has a face only a mother could love. He has got dimples. One of his eyes is bigger than the other.'
+  )
 
   useEffect(() => {
     setLocalPrompt(props.prompt)
@@ -111,7 +116,7 @@ export default function ChatDialog(props: Props) {
       <Modal isOpen={props.isOpen} onClose={props.onClose}>
         <ModalOverlay />
         <ModalContent height={560} maxWidth={850}>
-          <ModalBody p={6} className={styles['dialog-card']}>
+          <ModalBody pt={6} px={0} className={styles['dialog-card']}>
             <Flex justify="center" height="100%">
               <Grid
                 templateRows="repeat(1, 1fr)"
@@ -122,16 +127,44 @@ export default function ChatDialog(props: Props) {
                   rowSpan={1}
                   className={styles['model-view-environment-box']}
                 >
-                  <Editable
-                    value={localPrompt}
-                    onChange={(nextValue: string) => {
-                      setLocalPrompt(nextValue)
-                    }}
-                    className={styles['dialog-card-editable-text']}
-                  >
-                    <EditablePreview />
-                    <EditableTextarea />
-                  </Editable>
+                  <VStack>
+                    <Box width="100%">
+                      <Text
+                        className={styles['dialog-card-editable-heading']}
+                        mt={4}
+                        ml={7}
+                        mb={2}
+                      >
+                        Prompt
+                      </Text>
+                    </Box>
+                    <Editable
+                      value={localPrompt}
+                      onChange={(nextValue: string) => {
+                        setLocalPrompt(nextValue)
+                      }}
+                      className={styles['dialog-card-editable-text']}
+                    >
+                      <EditablePreview
+                        p={3}
+                        className={styles['dialog-card-editable-text']}
+                      />
+                      <EditableTextarea
+                        p={3}
+                        className={styles['dialog-card-editable-text']}
+                      />
+                    </Editable>
+                    <Button
+                      mt={3}
+                      onClick={props.onSubmit}
+                      borderRadius="20px"
+                      width="374px"
+                      background="#4A00E0"
+                      colorScheme={'purple'}
+                    >
+                      Generate
+                    </Button>
+                  </VStack>
                 </GridItem>
                 <GridItem colSpan={1} rowSpan={1}>
                   <Flex
@@ -147,7 +180,6 @@ export default function ChatDialog(props: Props) {
               </Grid>
             </Flex>
           </ModalBody>
-          <ModalFooter></ModalFooter>
         </ModalContent>
       </Modal>
     </LightMode>
