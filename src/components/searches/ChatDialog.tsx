6e57f0870931b2095e718f4c92a9af8c70e27596
yanglinshu <nicolasvandarkholme@outlook.com>
2023-02-24 14:00:40 +0800

    feat: disable sending empty chat

--- src/components/searches/ChatDialog.tsx
+++ src/components/searches/ChatDialog.tsx
@@ -238,6 +238,7 @@ export default function ChatDialog(props: Props) {
                       <ChatInputArea
                         {...props}
                         onSend={(msg: string) => {
+                          if (msg === '') return
                           scrollToBottom()
                           props.onSend(msg)
                         }}
